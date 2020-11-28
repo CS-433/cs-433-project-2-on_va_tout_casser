8bf679dfd45d114da65c367209b8bb0079c037fa
TheCrazyKing <lucas.meier@epfl.ch>
2020-11-28 16:51:33 +0100

    initiate branch

--- glove_template.py
+++ glove_template.py
@@ -3,11 +3,13 @@ from scipy.sparse import *
 import numpy as np
 import pickle
 import random
-
-
+import glove
+from gensim.models import word2vec
+import embeddings
+import fasttext
 def main():
     print("loading cooccurrence matrix")
-    with open('cooc.pkl', 'rb') as f:
+    with open('twitter-datasets\cooc.pkl', 'rb') as f:
         cooc = pickle.load(f)
     print("{} nonzero entries".format(cooc.nnz))
 
@@ -23,11 +25,21 @@ def main():
     alpha = 3 / 4
 
     epochs = 10
-
+    last = 3
+    print(cooc.row)
+    print(cooc.col)
+    print(cooc.data,len(list(zip(cooc.row, cooc.col, cooc.data))))
+    return
     for epoch in range(epochs):
         print("epoch {}".format(epoch))
         for ix, jy, n in zip(cooc.row, cooc.col, cooc.data):
-
+            #if( ix < last):
+               # print(ix,jy,n)
+            #last = ix
+            print(ix,jy,n,len(cooc.row),len(cooc.col),len(cooc.data))
+            # Left : is the line, i.e. which tweet it is
+            # Middle : is the word number of this tweet
+            # Right : is the number of occurences of this word over all tweets
 			# fill in your SGD code here, 
 			# for the update resulting from co-occurence (i,j)
 		
