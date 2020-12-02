f5887ed357fd68f4eda0760c68cb23119b9e89bc
Jonathan Reymond <jonrey@romandie.com>
2020-12-02 16:28:21 +0100

    added doc2vec method, TODO, set pickle to false

--- word2vec.py
+++ word2vec.py
@@ -6,6 +6,8 @@ import gensim
 import pickle
 import multiprocessing
 from gensim.models.word2vec import Word2Vec
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+import numpy as np
 
 path = 'dataset/'
 google_vector_size = 300
@@ -65,12 +67,6 @@ def extract_tweets_data(full, use_pickle):
     return tweets_labels, tweets_tokenized 
     
 
-def word2vec_model(tweets_tokenized, vector_size, window_size, epochs, seed, use_google, google_internet,google_use_pickle,full):
-    if use_google:
-        return word2vec_google_model(tweets_tokenized, google_internet, google_use_pickle,full)
-    else :
-        return word2vec_self_training_model(tweets_tokenized, vector_size, window_size, epochs, seed)
-
 
 def word2vec_google_model(tweets_tokenized, google_internet, google_use_pickle, full):
     #use per-trained word2vec model of google: 300 features per word: 3 millions words
@@ -80,9 +76,6 @@ def word2vec_google_model(tweets_tokenized, google_internet, google_use_pickle, 
     else: 
         model = gensim.models.KeyedVectors.load_word2vec_format('model/GoogleNews-vectors-negative300.bin', binary=True)
     print("Google model: Loading the model terminated")
-
-    
-
 
     tweets_cleaned = []
     if full: 
@@ -122,29 +115,60 @@ def word2vec_google_model(tweets_tokenized, google_internet, google_use_pickle, 
     return model, tweets_cleaned, google_vector_size
 
 
-
-def word2vec_self_training_model(tweets_tokenized, vector_size, window_size, epochs, seed):
-    print("training word2vec model...")
-    model = Word2Vec(sentences=tweets_tokenized,
-                    size=vector_size, 
-                    window=window_size, 
-                    iter=epochs,
-                    seed=seed,
-                    workers=multiprocessing.cpu_count())
-    print("training word2vec model terminated")
-    #save model
-    print("saving model")
-    model.save('model/model_word2vec')
-    print("saving model terminated")
-    #load model to continue to train
-    # new_model = Word2Vec.load('model/model_word2vec')
-    # model.train(more_sentences)
+# can train more sentences : model.train(more_sentences)
+def word2vec_self_training_model(tweets_tokenized, vector_size, window_size, epochs, seed, stored_model):
+    if stored_model:
+        model = Word2Vec.load('model/model_word2vec')
+    else:
+        print("training word2vec model...")
+        model = Word2Vec(sentences=tweets_tokenized,
+                        size=vector_size, 
+                        window=window_size, 
+                        iter=epochs,
+                        seed=seed,
+                        workers=multiprocessing.cpu_count())
+        print("training word2vec model terminated")
+        #save model
+        print("saving model")
+        model.save('model/model_word2vec')
+        print("saving model terminated")
 
     #fixing model to save space
     vectors = model.wv
     del model
     return vectors, tweets_tokenized, vector_size
-            
+
+
+
+def model_to_X(model, num_samples):
+    X = np.zeros((num_samples, vector_size))
+    for i in range(num_samples):
+        X[i, :] = model.docvecs[i]
+    return X
+
+#TODO: explain, https://radimrehurek.com/gensim/models/doc2vec.html
+# from the paper https://arxiv.org/pdf/1405.4053v2.pdf
+def doc2vec(tweets_tokenized, vector_size, window_size, epochs, seed, get_stored_model, PV_DM = 1):
+    if get_stored_model:
+        model = Doc2Vec.load('model/model_doc2vec')
+        return model_to_X(model, len(tweets_tokenized))
+
+    docs = [TaggedDocument(doc, [tag]) for tag, doc in enumerate(tweets_tokenized)]
+    print("training doc2vec model...")
+    model =  Doc2Vec(docs,
+                    dm=PV_DM, 
+                    vector_size=vector_size, 
+                    window=window_size, 
+                    seed=seed,
+                    epochs=epochs,
+                    workers=multiprocessing.cpu_count())
+    print("training doc2vec model terminated")
+
+    print("saving model")
+    model.save('model/model_doc2vec')
+    print("saving model terminated")
+
+    return model_to_X(model, len(tweets_tokenized))
 
 
 
@@ -152,21 +176,40 @@ if __name__ == '__main__':
     #data processing
     full_data = False
     #if want to extract tweets tokenized directly from pickle file
-    use_pickle = True
+    use_pickle = False
     tweets_labels, tweets_tokenized = extract_tweets_data(full=full_data, use_pickle=use_pickle)
 
+    #!!! use either model_word2vec or model_google, not both !!!
+    #Word2vec
+    get_stored_model = False
     #word2vec parameters
-    vector_size = 5       # ! using google pretrained model, the vector size is fixed at 300
+    vector_size = 5
     window_size = 10
     epochs = 2
     seed = 1
+    model_word2vec, tweets_cleaned, vector_size = word2vec_self_training_model(tweets_tokenized, 
+                                                                    vector_size, window_size, epochs, seed,
+                                                                    stored_model = get_stored_model)
 
-    use_google = False
-    #loading the google model from internet
+    # loading the google model from internet // using google pretrained model, the vector size is fixed at 300
     google_internet = False
     google_use_pickle = False
-    model, tweets_cleaned, vector_size = word2vec_model(tweets_tokenized, 
-                                                        vector_size, window_size, epochs, seed,
-                                                        use_google, google_internet, google_use_pickle, full_data)
+    model_google, tweets_cleaned, vector_size = word2vec_google_model(tweets_tokenized, 
+                                                        google_internet, google_use_pickle, full_data)
+
+
+    #Tweets to vectors
+    #1. using doc2vec !!! if so, comment word2vec model computation (line above)
+    doc2vec_epochs = 2
+    #use distributed memory = 1, bag-of-words = 0 see paper https://arxiv.org/pdf/1405.4053v2.pdf
+    PV_DM = 1
+    get_stored_model_doc2vec = False   
+    X = doc2vec(tweets_tokenized, vector_size, window_size, doc2vec_epochs, seed, 
+                get_stored_model = get_stored_model_doc2vec,PV_DM = PV_DM)
+
+    Y = np.array(tweets_labels).reshape((-1,1))
+    
+    print(X.shape)
+    print(Y.shape)
     
     
