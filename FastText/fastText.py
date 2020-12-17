36530fc0138512d45627eb3577b0d93e30119599
TheCrazyKing <lucas.meier@epfl.ch>
2020-12-17 14:59:42 +0100

    add validation

--- FastText/fastText.py
+++ FastText/fastText.py
@@ -3,10 +3,11 @@ import os.path
 import numpy as np
 from datetime import datetime
 import string
+
+useless = ['onomatopeia', 'abbreviation', 'repetition', 'misspelled']
 def preprocess(tweet):
-    tweet = tweet.replace('<user>','')
-    tweet = tweet.replace('<url>','')
-    tweet = "".join([char for char in tweet if char not in string.punctuation])
+    #tweet = tweet.replace('<user>','')
+    #tweet = tweet.replace('<url>','')
     return tweet
 
 
@@ -14,9 +15,9 @@ def preprocess(tweet):
 def addLabels(full=False):
     path = "twitter-datasets\\"
     if(full):
-        neg = "train_neg_full.txt"
-        pos = "train_pos_full.txt"
-        res = "res_fasttext_full.txt"
+        neg = "neg_with_abb_with_corr.txt"
+        pos = "pos_with_abb_with_corr.txt"
+        res = "COR_res_fasttext_full.txt"
     else:
         neg = "train_neg.txt"
         pos = "train_pos.txt"
@@ -25,26 +26,30 @@ def addLabels(full=False):
     if(os.path.isfile(path+res)):
         return
     else:
-        try:
+        # try:
             fileRes = open(path+res,"w",errors="namereplace")
             with open(path+neg,encoding='utf-8',errors="namereplace") as f:
                 for neg_line in f:
                     # try:
+                    #tweet = tweet.replace("\n","")
                     fileRes.write("__label__0 "+preprocess(neg_line))
                     # except UnicodeDecodeError:
                     #     print("Got an error",flush=True)
                     #     continue
             with open(path+pos,encoding="utf-8",errors="namereplace") as f:
+                #print("I pos line")
                 for pos_line in f:
+                    #print("I pos line2 ",flush=True)
+                   # print("writing ","__label__1 "+preprocess(tweet))
                     fileRes.write("__label__1 "+preprocess(pos_line))
-        except Exception :
-            print("Error occured",flush=True)
-            if(os.path.isfile(path+res)):
-                fileRes.close()
-                os.remove(path+res)
-        finally:
-            if(not fileRes.closed()):
-                fileRes.close()
+        # except Exception :
+        #     print("Error occured",flush=True)
+        #     if(os.path.isfile(path+res)):
+        #         fileRes.close()
+        #         os.remove(path+res)
+        # finally:
+        #     if(not fileRes.closed()):
+        #         fileRes.close()
 
 if __name__ == "__main__":
     full = True
@@ -52,14 +57,14 @@ if __name__ == "__main__":
     
     path = "twitter-datasets\\"
     if(full):
-        res = "res_fasttext_full.txt"
+        res = "COR_res_fasttext_full.txt"
     else:
-        res = "res_fasttext.txt"
+        res = "AUGMENT_res_fasttext.txt"
     model = fasttext.train_supervised(input = path+res,epoch = 25)
     try:
-        resFile = open("submission_"+str(full)+str(datetime.now()).replace(" ","__").replace(":","-")+".csv","w")
+        resFile = open("PREGOODsubmission_"+str(full)+str(datetime.now()).replace(" ","__").replace(":","-")+".csv","w")
         resFile.write("Id,Prediction\n")
-        with open(path+"test_data.txt") as f:
+        with open(path+"test_with_abb_with_corr.txt") as f:
             for line in f:
                 sep = line.find(",")
                 id_ = line[0:sep]
@@ -69,10 +74,10 @@ if __name__ == "__main__":
                 pred = model.predict(tweet)
                 if(pred[0][0] == "__label__0"):
                     pred = -1
-                elif(pred[0][0] == "__label__1"):
+                elif(pred[0][0]=="__label__1"):
                     pred = 1
                 else:
                     print("Not pred as predicted ! ",pred[0],pred[0][0])
-                resFile.write(str(id_)+","+str(pred)+"\n")
+                resFile.write(str(int(id_)+1)+","+str(pred)+"\n")
     finally:
         resFile.close()
